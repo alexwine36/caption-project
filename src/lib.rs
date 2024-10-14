c472b6b3620c49bed6296c3705d430364f130916
alexwine36 <alexwine36@gmail.com>
2024-10-14 11:07:40 -0600

    ci: test feature detection with probe ssl

--- src/lib.rs
+++ src/lib.rs
@@ -16,8 +16,24 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+#[cfg(features = "openssl-vendored")]
+use openssl_probe;
+
+use pyo3::prelude::*;
+
+#[cfg(features = "openssl-vendored")]
+fn probe_ssl_certs() {
+    openssl_probe::init_ssl_cert_env_vars();
+}
+
+#[cfg(not(openssl_probe))]
+fn probe_ssl_certs() {}
+
+
+
 #[pyfunction]
 fn caption_image(image_path: &str) -> PyResult<String> {
+    probe_ssl_certs();
     let res = caption::read_file(image_path);//.map_err(Err(PyValueError::new_err("")))
     match res {
         Ok(contents) => Ok(contents),
