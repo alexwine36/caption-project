bcf7949cf1d62fffdd3732fa38c17f23fef1a0ec
alexwine36 <alexwine36@gmail.com>
2024-10-14 13:20:49 -0600

    ci: update install script

--- scripts/install_openssl.sh
+++ scripts/install_openssl.sh
@@ -9,15 +9,23 @@ install_openssl() {
         if [ -f /etc/lsb-release ] && grep -q "Ubuntu" /etc/lsb-release; then
             # Ubuntu
             apt-get update
-            apt-get install -y openssl
+            apt-get install -y pkg-config libssl-dev
         else
             # Debian
             apt-get update
-            apt-get install -y openssl
+            apt-get install -y pkg-config libssl-dev
         fi
     elif [ -f /etc/fedora-release ]; then
         # Fedora
-        dnf install -y openssl
+        dnf install -y openssl openssl-devel
+    elif [ -f /etc/redhat-release ]; then
+        # CentOS/RHEL
+        if command -v dnf &> /dev/null; then
+            # Use DNF if available (CentOS 8+, RHEL 8+)
+            dnf install -y openssl openssl-devel
+        else
+            # Fall back to YUM for older versions
+            yum install -y openssl openssl-devel
     elif [ -f /etc/alpine-release ]; then
         # Alpine Linux
         apk add --no-cache openssl
